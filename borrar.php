5e11893488697e63831ed6de4b48841950846110
Joaquin Monge <joaquin.monge@iesdonana.org>
2020-10-26 19:22:29 +0100

    basededatos

---
+++ borrar.php
@@ -0,0 +1,18 @@
+
+<?php
+if (isset($_POST['id'])) {
+    $id = trim($_POST['id']);
+    $pdo = new PDO('pgsql:host:localhost;dbname=prueba', 'prueba', 'prueba');
+    $pdo->prepare('DELETE FROM depart WHERE id = :id');
+    $sent->execute([':id' => $id]);
+}
+
+header('Location: index.php');
+
+
+
+
+
+
+
+?>
--- prueba.sql
+++ prueba.sql
@@ -21,3 +21,12 @@ VALUES (10, 'CONTABILIDAD', 'SANLÚCAR')
      , (20, 'INVESTIGACIÓN', 'JEREZ')
      , (30, 'VENTAS', 'CHIPIONA')
      , (40, 'PRODUCCIÓN', 'TREBUJENA');
+
+  
+-- Conversiones entre SQL y PHP
+-- NULL <-> null
+-- BOOL <-> bool
+-- entero (no numeric) <-> int (si cabe en un int de php)
+-- el resto <-> string
+
+
